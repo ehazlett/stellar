2b5677dbe64202fca4d926bffe2be67f12e33d17
Evan Hazlett <ejhazlett@gmail.com>
2018-06-10 00:23:58 -0400

    vendor update
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- vendor/github.com/ehazlett/element/shutdown.go
+++ vendor/github.com/ehazlett/element/shutdown.go
@@ -1,17 +1,11 @@
 package element
 
-import (
-	"github.com/sirupsen/logrus"
-)
-
 // Shutdown causes the local node to leave the cluster and perform a clean shutdown
 func (a *Agent) Shutdown() error {
-	logrus.Debug("agent leave")
 	if err := a.members.Leave(nodeUpdateTimeout); err != nil {
 		return err
 	}
 
-	logrus.Debug("agent shutdown")
 	if err := a.members.Shutdown(); err != nil {
 		return err
 	}
