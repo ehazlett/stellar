f3b89bc976e5561c265db632dddedb8a29c59dd3
Evan Hazlett <ejhazlett@gmail.com>
2018-08-23 20:27:15 +0000

    add lookup to ns client
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- services/proxy/reload.go
+++ services/proxy/reload.go
@@ -2,9 +2,15 @@ package proxy
 
 import (
 	"context"
+	"fmt"
+	"net/url"
+	"reflect"
+	"strings"
 
+	nameserverapi "github.com/ehazlett/stellar/api/services/nameserver/v1"
 	api "github.com/ehazlett/stellar/api/services/proxy/v1"
 	ptypes "github.com/gogo/protobuf/types"
+	"github.com/sirupsen/logrus"
 )
 
 func (s *service) Reload(ctx context.Context, req *api.ReloadRequest) (*ptypes.Empty, error) {
@@ -26,9 +32,89 @@ func (s *service) reload() error {
 		return err
 	}
 
-	if err := s.proxy.Reload(apps); err != nil {
+	next := map[string]*backend{}
+	appServers := map[string][]*url.URL{}
+
+	for _, app := range apps {
+		for _, svc := range app.Services {
+			if len(svc.Endpoints) == 0 {
+				continue
+			}
+			// parse url and notify update
+			for _, ep := range svc.Endpoints {
+				if strings.ToLower(ep.Protocol.String()) != "http" {
+					logrus.Warnf("proxy: unsupported protocol %s for endpoint %s", ep.Protocol, ep.Service)
+					continue
+				}
+				// lookup the ip for faster resolves
+				records, err := c.Nameserver().Lookup(svc.Name)
+				if err != nil {
+					logrus.Warnf("proxy: unable to lookup service address: %s", err)
+					continue
+				}
+				servers := []*url.URL{}
+				for _, record := range records {
+					// only use A records
+					if record.Type != nameserverapi.RecordType_A {
+						continue
+					}
+					servers = append(servers, &url.URL{
+						Scheme: "http",
+						Host:   fmt.Sprintf("%s:%d", record.Value, ep.Port),
+						Path:   "/",
+					})
+				}
+				appServers[ep.Host] = append(appServers[ep.Host], servers...)
+			}
+		}
+	}
+
+	updates := []*proxyUpdate{}
+
+	// trigger updates
+	for host, servers := range appServers {
+		serverID, err := s.generateServerID(host)
+		if err != nil {
+			logrus.Errorf("proxy: error generating server id: %s", err)
+			continue
+		}
+
+		b, exists := s.currentServers[serverID]
+		if !exists {
+			lb, err := newLB()
+			if err != nil {
+				logrus.Errorf("proxy: error setting up backend: %s", err)
+				continue
+			}
+			b = &backend{
+				host:    host,
+				lb:      lb,
+				servers: servers,
+			}
+		}
+
+		update := &proxyUpdate{
+			backend: b,
+		}
+		update.action = s.getUpdateAction(serverID)
+		updates = append(updates, update)
+		next[serverID] = b
+	}
+
+	// nothing changed; skip
+	if reflect.DeepEqual(next, s.currentServers) {
 		return nil
 	}
+
+	// notify updates
+	for _, update := range updates {
+		s.updateCh <- update
+	}
+
+	// prune
+	s.pruneServers(next)
+	// update current servers
+	s.currentServers = next
 
 	return nil
 
--- services/proxy/service.go
+++ services/proxy/service.go
@@ -1,7 +1,13 @@
 package proxy
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/containerd/containerd"
@@ -10,7 +16,11 @@ import (
 	api "github.com/ehazlett/stellar/api/services/proxy/v1"
 	"github.com/ehazlett/stellar/client"
 	ptypes "github.com/gogo/protobuf/types"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"github.com/vulcand/oxy/forward"
+	"github.com/vulcand/oxy/roundrobin"
+	"github.com/vulcand/route"
 	"google.golang.org/grpc"
 )
 
@@ -26,21 +36,50 @@ type service struct {
 	containerdAddr string
 	namespace      string
 	agent          *element.Agent
-	proxy          *Proxy
+	proxyHTTPPort  int
+	errCh          chan error
+	updateCh       chan *proxyUpdate
+	currentServers map[string]*backend
+	mux            *route.Mux
+}
+
+type backend struct {
+	host    string
+	lb      *roundrobin.RoundRobin
+	servers []*url.URL
+}
+
+type updateAction string
+
+const (
+	updateActionAdd    updateAction = "add"
+	updateActionUpdate updateAction = "update"
+	updateActionRemove updateAction = "remove"
+)
+
+type proxyUpdate struct {
+	action  updateAction
+	backend *backend
 }
 
 func New(cfg *stellar.Config, agent *element.Agent) (*service, error) {
-	p, err := NewProxy(&Config{
-		HTTPPort: cfg.ProxyHTTPPort,
-	})
-	if err != nil {
-		return nil, err
-	}
+	errCh := make(chan error)
+	go func() {
+		for {
+			err := <-errCh
+			logrus.Errorf("proxy: %s", err)
+		}
+	}()
+
 	return &service{
 		containerdAddr: cfg.ContainerdAddr,
 		namespace:      cfg.Namespace,
+		proxyHTTPPort:  cfg.ProxyHTTPPort,
+		errCh:          errCh,
+		updateCh:       make(chan *proxyUpdate),
+		currentServers: make(map[string]*backend),
+		mux:            route.NewMux(),
 		agent:          agent,
-		proxy:          p,
 	}, nil
 }
 
@@ -54,6 +93,13 @@ func (s *service) ID() string {
 }
 
 func (s *service) Start() error {
+	go s.updater()
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", s.proxyHTTPPort),
+		Handler: s.mux,
+	}
+
 	t := time.NewTicker(5 * time.Second)
 	go func() {
 		for range t.C {
@@ -62,7 +108,14 @@ func (s *service) Start() error {
 			}
 		}
 	}()
-	return s.proxy.Run()
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			logrus.Error(errors.Wrap(err, "proxy"))
+		}
+	}()
+
+	return nil
 }
 
 func (s *service) containerd() (*containerd.Client, error) {
@@ -109,4 +162,59 @@ func (s *service) nodeClient(id string) (*client.Client, error) {
 	}
 
 	return nil, fmt.Errorf("node %s not found in cluster", id)
+}
+
+func newLB() (*roundrobin.RoundRobin, error) {
+	// TODO: log separately?
+	l := logrus.New()
+	l.Out = ioutil.Discard
+	fwd, err := forward.New(forward.Logger(l))
+	if err != nil {
+		return nil, err
+	}
+	lb, err := roundrobin.New(fwd, roundrobin.RoundRobinLogger(l))
+	if err != nil {
+		return nil, err
+	}
+
+	return lb, nil
+}
+
+func (s *service) getUpdateAction(id string) updateAction {
+	if _, ok := s.currentServers[id]; ok {
+		return updateActionUpdate
+	}
+
+	return updateActionAdd
+}
+
+func (s *service) generateServerID(v interface{}, extra ...interface{}) (string, error) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+	h := sha1.New()
+	h.Write(data)
+	for _, x := range extra {
+		d, err := json.Marshal(x)
+		if err != nil {
+			return "", err
+
+		}
+		h.Write(d)
+	}
+	r := hex.EncodeToString(h.Sum(nil))[:24]
+	return r, nil
+}
+
+func (s *service) pruneServers(next map[string]*backend) {
+	for k, b := range s.currentServers {
+		if _, exists := next[k]; !exists {
+			logrus.Debugf("proxy: %s not found; removing", k)
+			s.updateCh <- &proxyUpdate{
+				backend: b,
+				action:  updateActionRemove,
+			}
+		}
+	}
 }
